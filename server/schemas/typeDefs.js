6df05ee70c74d26cfdd7dad2ec3a5e6fbcc73711
mloeff <michaelloeffler23@gmail.com>
2023-05-04 19:44:36 -0500

    creates getEvents query, signup/login mutations, and CRUD operator mutations for events

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,11 +26,16 @@ const typeDefs = gql`
   }
 
   type Query {
-  
+    getEvents(email: String!): User
+
   }
 
   type Mutation {
-  
+    signup(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addEvent(title: String!, date: Date!, startTime: Date!, endTime: Date!, location: String, description: String): Event
+    removeEvent(eventId: ID!): Event
+    updateEvent(eventId: ID!, title: String, date: Date, startTime: Date, endTime: Date, location: String, description: String)
   }
 `;
 
