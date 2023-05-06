9a5fedbcf522d31d351f3afde2563b3defbfca95
mloeff <michaelloeffler23@gmail.com>
2023-05-06 12:38:28 -0500

    adds starter code for calendar component

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from "react";
 
-import { Calendar, dayjsLocalizer } from 'react-big-calendar'
+import { Calendar, Views, dayjsLocalizer } from 'react-big-calendar'
 import dayjs from 'dayjs'
 
 const localizer = dayjsLocalizer(dayjs)
 
-
-
 function App() {
 
+  return (
+    <div className="height600">
+      <Calendar
+        // components={components}
+        // defaultDate={defaultDate}
+        // events={events}
+        localizer={localizer}
+        showMultiDayTimes
+        step={60}
+        // views={views}
+        // events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        // style={{ height: 500 }}
+      />
+    </div>
+  )
+  
 
 }
  
