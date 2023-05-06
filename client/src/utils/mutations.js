55f12cda7d387f22755afd9c46ac6175cdb04f01
unknown <maria.rodriguez@northwestern.edu>
2023-05-06 10:40:11 -0500

    update and delete events

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,7 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation signup ($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+  mutation signup($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
     signup(firstName: $firstName, lastNAme: $lastName, email: $email, password: $password) {
       token
       user {
@@ -25,17 +25,25 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_EVENT = gql`
-  mutation addEvent ($title: String!, $date: Date!, $startTime: Date!, $endTime: Date!, $location: String, $description: String) {
-    addEvent( title: $title, date: $date, startTime: $startTime, endTime: $endTime, location: $location, description: $description) {
-      _id
-      title
-      date
-      startTime
-      endTime
-      location
-      description
+  mutation addEvent($title: String!, $date: Date!, $startTime: Date!, $endTime: Date!, $location: String, $description: String) {
+    addEvent(title: $title, date: $date, startTime: $startTime, endTime: $endTime, location: $location, description: $description) {
+      event
     }
   }
 `;
 
+export const REMOVE_EVENT = gql`
+  mutation removeEvent($eventId: ID!) {
+  removeEvent(eventId: $eventId) {
+    event 
+    }
+  } 
+`;
 
+export const UPDATE_EVENT = gql`
+  mutation updateEvent($eventId: ID!, $title: String, $date: Date, $startTime: Date, $endTime: Date, $location: String, $description: String) {
+    updateEvent(eventId: $eventId, title: $title, date: $date, startTime: $startTime, endTime: $endTime, location: $location, description: $description) {
+      event
+    }
+  }
+`;
