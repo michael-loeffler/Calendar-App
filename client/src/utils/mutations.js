ce7acf9ce7cd38f2f713725ec79a37d1588a1a21
michael-loeffler <117034695+michael-loeffler@users.noreply.github.com>
2023-05-10 21:26:51 -0500

    Update mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -55,7 +55,7 @@ export const REMOVE_EVENT = gql`
 `;
 
 export const UPDATE_EVENT = gql`
-  mutation updateEvent($title: String!, $start: Date!, $end: Date!, $location: String, $description: String, $allDay: Boolean, $color: Strin) {
+  mutation updateEvent($title: String!, $start: Date!, $end: Date!, $location: String, $description: String, $allDay: Boolean, $color: String) {
     updateEvent(title: $title, start: $start, end: $end, location: $location, description: $description, allDay: $allDay, color: $color) {
       _id
       title
